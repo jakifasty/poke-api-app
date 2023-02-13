fac1468a97b19f55b30d2a3be9c63bfa1137b7b5
Borja Javierre <borja.general@gmail.com>
2023-02-14 00:56:13 +0100

    add useStates for prev, next and current pages and pages setters

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import PokemonList from './PokemonList';
-import axios from 'axios'; //for 
+import axios from 'axios'; //allows to fetch from APIs easier that using the deafult FETCH from bowser
 
 function App() {
 
-  const [pokemon, setPokemon] = useState(["charmander", "bulbasur"])
+  const [pokemon, setPokemon] = useState([])
+  const [previousPageURL, setPreviousPageURL] = useState()
+  const [currentPageURL, setCurrentPageURL] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageURL, setNextPageURL] = useState()
+  const [loading, setLoading] = useState(true)
 
-  axios.get()
+  useEffect(() => {
+    setLoading(true)
+    //this is an effect, useEffect is being hooked up properly with axios
+    axios.get(currentPageURL).then(res => {
+      setLoading(false)
+      setNextPageURL(res.data.next)
+      setPreviousPageURL(res.data.previous)
+      setPokemon(res.data.results.map(pok => pok.name))
+  })
+  }, [currentPageURL]) //every time URL chnages, rerun the URL
+
+  if(loading) return "Loading..."
 
   return (
     <PokemonList pokemon={pokemon}/>
