8219c3da4fabc74cd81b6bf63e205a4a8927d6e8
Borja Javierre <borja.general@gmail.com>
2023-02-14 01:16:50 +0100

    add functions previous and next page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import PokemonList from './PokemonList';
 import axios from 'axios'; //allows to fetch from APIs easier that using the deafult FETCH from bowser
+import Pagination from './Pagination';
 
 function App() {
 
@@ -12,20 +13,35 @@ function App() {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
+    let cancel
     setLoading(true)
     //this is an effect, useEffect is being hooked up properly with axios
-    axios.get(currentPageURL).then(res => {
+    axios.get(currentPageURL, {
+      cancelToken: new axios.CancelToken(c => cancel = c )
+    }).then(res => {
       setLoading(false)
       setNextPageURL(res.data.next)
       setPreviousPageURL(res.data.previous)
       setPokemon(res.data.results.map(pok => pok.name))
   })
+  return () => cancel() 
   }, [currentPageURL]) //every time URL chnages, rerun the URL
 
   if(loading) return "Loading..."
 
-  return (
-    <PokemonList pokemon={pokemon}/>
+  function goToNextPage() { //pagination
+    setCurrentPageURL(nextPageURL)
+  }
+
+  function goToPreviousPage() {
+    setCurrentPageURL(previousPageURL)
+  }
+
+  return ( //we use <></> to return empty object 
+    <>
+      <PokemonList pokemon={pokemon}/>
+      <Pagination goToNextPage={goToNextPage} goToPreviousPage={goToPreviousPage}/>
+    </>
   );
 }
 
